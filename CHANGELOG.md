cd928ea4d2517f38e38897cb11c63d7e0817c4ba
ASPNyan <sebastian.ricketts2210@gmail.com>
2023-12-23 17:24:08 +1030

    Update to 0.0.2, hotfix for 0.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+﻿## 0.0.2 (Hotfix)
+Released hotfix for 0.0.1 so the mod actually works. Mod is now required on both the host *and* the clients.
+## 0.0.1 (Initial)
+Released mod, but it didn't work lmao
--- Plugin.cs
+++ Plugin.cs
@@ -1,6 +1,9 @@
-﻿using BepInEx;
+﻿using System.Diagnostics.CodeAnalysis;
+using BepInEx;
+using BepInEx.Logging;
 using GameNetcodeStuff;
 using HarmonyLib;
+using Unity.Netcode;
 using static LCDeathMessages.PluginInfo;
 
 namespace LCDeathMessages;
@@ -9,6 +12,7 @@ namespace LCDeathMessages;
 public class Plugin : BaseUnityPlugin
 {
     public static StartOfRound RoundData => StartOfRound.Instance;
+    [NotNull] public static ManualLogSource? Log { get; private set; }
 
     public static Plugin? Instance;
     private readonly Harmony _harmony = new(PLUGIN_GUID);
@@ -18,15 +22,16 @@ public class Plugin : BaseUnityPlugin
         Instance ??= this;
         Logger.LogInfo("Started Death Messages Plugin Successfully.");
         _harmony.PatchAll(typeof(Plugin));
+        Log = Logger;
     }
 
     // ReSharper disable once InconsistentNaming | Harmony requires double underscore prefix for injected values.
     [HarmonyPatch(typeof(PlayerControllerB), nameof(PlayerControllerB.KillPlayer)), HarmonyPostfix]
     private static void SendDeathMessage(PlayerControllerB __instance)
     {
-        const string terrariaRed = "#E11919FF";
-
-        if (__instance is not { IsOwner: true, isPlayerDead: true }) return;
+        const string terrariaRed = "#E11919";
+        
+        if (!__instance.isPlayerDead) return;
         
         string deathMessage = DeathMessages.Get(__instance.causeOfDeath, __instance.playerUsername);
         string colored = $"<color={terrariaRed}>{deathMessage}</color>";
